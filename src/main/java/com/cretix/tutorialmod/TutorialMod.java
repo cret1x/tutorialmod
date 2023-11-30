3a8ca277179c109d4c141f93abf840b3de3693b7
cret1x <33805149+cret1x@users.noreply.github.com>
2023-11-30 16:43:42 +0300

    Added ores, loot tables and custom item and block

--- src/main/java/com/cretix/tutorialmod/TutorialMod.java
+++ src/main/java/com/cretix/tutorialmod/TutorialMod.java
@@ -5,6 +5,7 @@ import com.cretix.tutorialmod.item.ModItemGroups;
 import com.cretix.tutorialmod.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.fabric.api.registry.FuelRegistry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -18,5 +19,7 @@ public class TutorialMod implements ModInitializer {
 
 		ModItems.registerModItems();
 		ModBlocks.registerModBlocks();
+
+		FuelRegistry.INSTANCE.add(ModItems.COAL_BRIQUETTE, 200);
 	}
 }
--- src/main/java/com/cretix/tutorialmod/item/ModItems.java
+++ src/main/java/com/cretix/tutorialmod/item/ModItems.java
@@ -1,6 +1,7 @@
 package com.cretix.tutorialmod.item;
 
 import com.cretix.tutorialmod.TutorialMod;
+import com.cretix.tutorialmod.item.custom.MetalDetectorItem;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
@@ -14,6 +15,11 @@ import net.minecraft.util.Identifier;
 public class ModItems {
     public static final Item RUBY = registerItem("ruby", new Item(new FabricItemSettings()));
     public static final Item RAW_RUBY = registerItem("raw_ruby", new Item(new FabricItemSettings()));
+    public static final Item TOMATO = registerItem("tomato", new Item(new FabricItemSettings().food(ModFoodComponents.TOMATO)));
+    public static final Item COAL_BRIQUETTE = registerItem("coal_briquette", new Item(new FabricItemSettings()));
+    public static final Item METAL_DETECTOR = registerItem("metal_detector",
+            new MetalDetectorItem(new FabricItemSettings().maxDamage(64)));
+
 
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registries.ITEM, new Identifier(TutorialMod.MOD_ID, name), item);
